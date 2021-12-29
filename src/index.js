e5234c0649bbfe0d385010e2bac9163c139288a6
John <johndlee58@gmail.com>
2021-12-28 21:03:51 -0500

    made rViewer appear in the panel. Todo: fix title.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import {
     provideFluentDesignSystem,
     fluentCombobox,
     fluentOption,
+    fluentTab,
+    fluentTabPanel,
+    fluentTabs,
 } from '@fluentui/web-components'
 import renderSelectorPanel from './selectorPanel'
-// import renderMainPanel from './mainPanel'
+import renderMainPanel from './mainPanel'
 import testDataCatalog from '../test/selectorPanel/testDataCatalog'
 import './global.scss'
 const selectorPanel = document.createElement('div')
@@ -14,12 +17,19 @@ const mainPanel = document.createElement('div')
 mainPanel.setAttribute('id', 'main-panel')
 document.body.appendChild(selectorPanel)
 document.body.appendChild(mainPanel)
-provideFluentDesignSystem().register(fluentCombobox(), fluentOption())
+provideFluentDesignSystem().register(
+    fluentCombobox(),
+    fluentOption(),
+    fluentTab(),
+    fluentTabPanel(),
+    fluentTabs()
+)
+
 renderSelectorPanel(testDataCatalog)
 // where event listeners should be added.
 
 document.getElementById('fetch-button').addEventListener('click', () => {
-    alert('it works')
+    // alert('it works')
     let a = document
         .getElementById('subjectSelectize')
         .getAttribute(`current-value`)
@@ -30,5 +40,13 @@ document.getElementById('fetch-button').addEventListener('click', () => {
     let c = testDataCatalog.Networks[a][b]
     console.log(b)
     console.log(c)
+    renderMainPanel(a,b)
+    // remove all child elements of the selector panel.
+    while (selectorPanel.firstChild) {
+        selectorPanel.removeChild(selectorPanel.firstChild)
+    }
+    //todo: pick work back up here.
+    let aSearch=0
+    alert(testDataCatalog[0])
+
 })
-// renderMainPanel()
--- src/mainPanel/index.js
+++ src/mainPanel/index.js
@@ -11,9 +11,37 @@ export default () => {
     const mainTitleBar = document.createElement('div')
     mainTitleBar.innerHTML = `<h1>00010101 Title</h1>`
     mainPanel.appendChild(mainTitleBar)
+    const mainPanelTabs = document.createElement('div')
+    mainPanelTabs.innerHTML = `<fluent-tabs activeid="entrees">
+    <fluent-tab id="presentation">Presentation</fluent-tab>
+    <fluent-tab id="definition">Definition</fluent-tab>
+    <fluent-tab id="calculation">Calculation</fluent-tab>
+    <fluent-tab-panel id="presentationPanel">
+    </fluent-tab-panel>
+    <fluent-tab-panel id="definitionPanel">
+        <ol>
+            <li><fluent-anchor href="#" appearance="hypertext">Mushroom-Sausage Ragù</fluent-anchor></li>
+            <li><fluent-anchor href="#" appearance="hypertext">Tomato Bread Soup with Steamed Mussels</fluent-anchor></li>
+            <li><fluent-anchor href="#" appearance="hypertext">Grilled Fish with Artichoke Caponata</-anchor></li>
+            <li><fluent-anchor href="#" appearance="hypertext">Celery Root and Mushroom Lasagna</fluent-anchor></li>
+            <li><fluent-anchor href="#" appearance="hypertext">Osso Buco with Citrus Gremolata</fluent-anchor></li>
+        </ol>
+    </fluent-tab-panel>
+    <fluent-tab-panel id="calculationPanel">
+        <ol>
+            <li><fluent-anchor href="#" appearance="hypertext">Tiramisu</fluent-anchor></li>
+            <li><fluent-anchor href="#" appearance="hypertext">Spumoni</fluent-anchor></li>
+            <li><fluent-anchor href="#" appearance="hypertext">Limoncello and Ice Cream with Biscotti</fluent-anchor></li>
+        </ol>
+    </fluent-tab-panel>
+</fluent-tabs>
+    `
+    mainPanel.appendChild(mainPanelTabs)
+    const presentationPanel = document.getElementById('presentationPanel')
     const rViewer = document.createElement('div')
+    presentationPanel.appendChild(rViewer)
     rViewer.setAttribute('id', 'r-viewer')
-    mainPanel.appendChild(rViewer)
+    //mainPanel.appendChild(rViewer)
     renderPGridViewer(testDataPGrid)
     // renderArcDiagram(testDataArc)
 }
