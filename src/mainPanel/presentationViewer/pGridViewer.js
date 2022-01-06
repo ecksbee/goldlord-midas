8779cda024dd3a0684b7882bd25d38c4bb045872
John <johndlee58@gmail.com>
2022-01-05 22:26:42 -0500

    now rendering definition data with grid attribs.

--- src/mainPanel/presentationViewer/pGridViewer.js
+++ src/mainPanel/presentationViewer/pGridViewer.js
@@ -14,10 +14,28 @@ export default rawData => {
     rViewer.appendChild(pGridDiv)
 
     setTimeout(() => {
-        const grid = canvasDatagrid()
+        const grid = canvasDatagrid({
+            allowSorting: false, // affected by this bug https://github.com/TonyGermaneri/canvas-datagrid/issues/261
+            allowColumnReordering: false,
+            autoResizeColumns: true,
+            editable: false,
+            allowFreezingColumns: true,
+            allowFreezingRows: true,
+        })
+
+        // grid.fitColumnToValues('a')
         pGridDiv.appendChild(grid)
-        grid.style.height = '100%'
-        grid.style.width = '100%'
+        grid.style.height = '95%'
+        grid.style.width = '95%'
         grid.data = data
+        grid.frozenColumn = 1
+        let numFrozenRows = rawData.VoidQuadrant.length + 1
+        grid.frozenRow = numFrozenRows
+        // grid.fitColumnToValues()
+        grid.addEventListener('beforesortcolumn', e => {
+            e.preventDefault()
+        })
+        // let aWidth = grid.findColumnMaxTextLength('A')
+        // console.log(aWidth)
     }, 100)
 }
