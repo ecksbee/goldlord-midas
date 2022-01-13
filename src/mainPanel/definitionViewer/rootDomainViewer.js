47b78eb8299c7781fd3141f847b8a78f5e1374c7
John <johndlee58@gmail.com>
2022-01-12 21:22:33 -0500

    got arc diagram to display in definition tab.

--- src/mainPanel/definitionViewer/rootDomainViewer.js
+++ src/mainPanel/definitionViewer/rootDomainViewer.js
@@ -3,6 +3,9 @@
 import canvasDatagrid from 'canvas-datagrid'
 import transformRootDomain from './transformRootDomain'
 
+import arcDiagramTestData from '../../../test/mainPanel/definitionViewer/testDataArcDiagram'
+import renderArcDiagram from './arcDiagram'
+
 export default rawData => {
     const data = transformRootDomain(rawData)
     const rViewer2 = document.getElementById('r-viewerDefinition')
@@ -34,5 +37,41 @@ export default rawData => {
         grid.addEventListener('beforesortcolumn', e => {
             e.preventDefault()
         })
+        grid.addEventListener('contextmenu', e => {
+            e.items.push({
+                // dimensional relationship set
+                title: 'Visualize DRS',
+                click: () => {
+                    // TODO: MAKE THE CONTEXT MENU GO AWAY AFTER THIS IS CLICKED.
+                    mountArcDiagram()
+                    document.getElementById(
+                        'r-viewerDefinition'
+                    ).style.display = 'none'
+                    return
+                },
+            })
+        })
     }, 100)
+}
+
+const mountArcDiagram = e => {
+    const definitionPanel = document.getElementById('definitionPanel')
+    let arcDiagramDiv = document.getElementById('arcDiagramDiv') // if it isn't on the page yet, returns null and evaluates to falsey.
+    if (arcDiagramDiv) {
+        arcDiagramDiv.style.display = 'block'
+    } else {
+        arcDiagramDiv = document.createElement('div')
+        arcDiagramDiv.setAttribute('id', 'arcDiagramDiv')
+        let returnToRootDomainViewerButton = document.createElement('button')
+        returnToRootDomainViewerButton.innerText =
+            'Return to Root Domain Viewer'
+        arcDiagramDiv.appendChild(returnToRootDomainViewerButton)
+        returnToRootDomainViewerButton.addEventListener('click', e => {
+            document.getElementById('r-viewerDefinition').style.display =
+                'block'
+            document.getElementById('arcDiagramDiv').style.display = 'none'
+        })
+        definitionPanel.appendChild(arcDiagramDiv)
+        renderArcDiagram(arcDiagramTestData)
+    }
 }
