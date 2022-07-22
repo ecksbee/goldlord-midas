1e8b255ddc6a0212c2a65bba4f2315a2c82e14bf
Joshua Nario <joshuanario@users.noreply.github.com>
2022-07-22 17:11:09 -0400

    arcDiagramVertical

--- src/lib/arcDiagramVertical.js
+++ src/lib/arcDiagramVertical.js
@@ -6,7 +6,30 @@ export default (data, mount) => {
     const height = (data.nodes.length - 1) * step + margin.top + margin.bottom
     const y = d3.scalePoint(data.nodes.map(d => d.id).sort(d3.ascending), [margin.top, height - margin.bottom])
     const color = d3.scaleOrdinal(data.nodes.map(d => d.id).sort(d3.ascending), d3.schemeCategory10)
-
+    const graph = (() => {
+      const nodes = data.nodes.map(({id, group}) => ({
+        id,
+        sourceLinks: [],
+        targetLinks: [],
+        group
+      }));
+    
+      const nodeById = new Map(nodes.map(d => [d.id, d]));
+    
+      const links = data.links.map(({source, target, value}) => ({
+        source: nodeById.get(source),
+        target: nodeById.get(target),
+        value
+      }));
+    
+      for (const link of links) {
+        const {source, target, value} = link;
+        source.sourceLinks.push(link);
+        target.targetLinks.push(link);
+      }
+    
+      return {nodes, links};
+    })()
     function arc(d) {
         const y1 = d.source.y;
         const y2 = d.target.y;
@@ -47,7 +70,7 @@ export default (data, mount) => {
         .attr("font-size", 10)
         .attr("text-anchor", "end")
         .selectAll("g")
-        .data(data.nodes)
+        .data(graph.nodes)
         .join("g")
         .attr("transform", d => `translate(${margin.left},${d.y = y(d.id)})`)
         .call(g => g.append("text")
@@ -64,16 +87,16 @@ export default (data, mount) => {
         .attr("stroke-opacity", 0.6)
         .attr("stroke-width", 1.5)
         .selectAll("path")
-        .data(data.links)
+        .data(graph.links)
         .join("path")
-        .attr("stroke", d => d.source.group === d.target.group ? color(d.source.group) : "#aaa")
+        .attr("stroke", d => d.source.id === d.target.id ? color(d.source.id) :  "#aaa")
         .attr("d", arc);
       
     const overlay = svg.append("g")
         .attr("fill", "none")
         .attr("pointer-events", "all")
         .selectAll("rect")
-        .data(data.nodes)
+        .data(graph.nodes)
         .join("rect")
         .attr("width", margin.left + 40)
         .attr("height", step)
@@ -90,28 +113,5 @@ export default (data, mount) => {
             label.classed("secondary", false);
             path.classed("primary", false).order();
         });
-      
-    function update() {
-        y.domain(data.nodes.sort((a,b)=>a.name.localeCompare(b.name)).map(d => d.id));
-      
-        const t = svg.transition()
-            .duration(750);
-      
-        label.transition(t)
-            .delay((d, i) => i * 20)
-            .attrTween("transform", d => {
-            const i = d3.interpolateNumber(d.y, y(d.id));
-            return t => `translate(${margin.left},${d.y = i(t)})`;
-        });
-      
-        path.transition(t)
-            .duration(750 + data.nodes.length * 20)
-            .attrTween("d", d => () => arc(d));
-      
-        overlay.transition(t)
-            .delay((d, i) => i * 20)
-            .attr("y", d => y(d.id) - step / 2);
-        }
-        update()
       
 }
