300028002d5504a08d1737832b45d811be2354b6
Joshua Nario <joshuanario@users.noreply.github.com>
2024-01-16 17:47:28 -0500

    narrative fact bugfix

--- src/lib/store.js
+++ src/lib/store.js
@@ -80,10 +80,11 @@ const narrativeFactInnerHtml = () => {
         if (state.renderable) {
             const { rowIndex, columnIndex, linkbase } = state.narrativeFact
             const fact = state.renderable[linkbase].FactualQuadrant[rowIndex][columnIndex]
-            return fact?.[state.lang]?.InnerHtml || fact.Unlabelled.InnerHtml
+            return fact?.[state.lang]?.InnerHtml || fact?.Unlabelled.InnerHtml
         }
         if (state.expressable) {
-            return state.expressable.Expression?.[state.lang].InnerHtml
+            const expression = state.expressable.Expression
+            return expression?.[state.lang]?.InnerHtml || expression?.Unlabelled.InnerHtml
         }
     }
     return null
